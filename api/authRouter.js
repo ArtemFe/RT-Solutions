b836a8eae6d6db61ae5edf06a40d02a51afed202
A90 <vladsmertdizormafiy@gmail.com>
2025-03-16 01:00:09 +0500

    update

---
+++ api/authRouter.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('./authController')
+const {check} = require("express-validator")
+const authMiddleware = require('./middleware/authMiddleware')
+const roleMiddleware = require('./middleware/roleMiddleware')
+const path = require('path');
+
+const staticUrl = '../client';
+
+router.use(express.static(path.join(__dirname, staticUrl)));
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, staticUrl, 'html', 'index.html'));
+});
+
+router.get('/reg', (req, res) => {
+    res.sendFile(path.join(__dirname, staticUrl, 'html', 'register.html'));
+});
+
+router.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, staticUrl, 'html', 'login.html'));
+});
+
+router.get('/catalog', (req, res) => {
+    res.sendFile(path.join(__dirname, staticUrl, 'html', 'catalog.html'));
+});
+
+router.get('/product', (req, res) => {
+    res.sendFile(path.join(__dirname, staticUrl, 'html', 'product.html'));
+});
+
+router.post('/api/reg', [
+    check('username', "Имя пользователя не может быть пустым").notEmpty(),
+    check('password', "Пароль должен быть больше 8 символов").isLength({min:8})
+], controller.reg)
+router.post('/api/login', controller.login)
+router.get('/api/users', roleMiddleware(["Admin"]), controller.getUsers)
+
+module.exports = router
---
+++ client/js/login.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const apiUrl = 'http://localhost:5000/api';
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('loginForm').addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+    
+        try {
+            const response = await fetch(`${apiUrl}/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, password }),
+            });
+    
+            const result = await response.json();
+            if (result.message) {
+                console.log('Сообщение от сервера:', result.message);
+            }
+            if (result.token) {
+                console.log('Токен:', result.token);
+                localStorage.setItem('token', result.token);
+            } else {
+                console.log('Токен не найден в ответе сервера');
+            }
+        } catch (error) {
+            console.error('Ошибка:', error);
+            alert('Произошла ошибка при авторизации');
+        }
+    });    
+})
