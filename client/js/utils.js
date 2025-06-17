bec829ca367029d14954686ac2b6ed2e1d1dfcd0
A90 <vladsmertdizormafiy@gmail.com>
2025-06-18 02:57:13 +0500

    all project

--- client/js/utils.js
+++ client/js/utils.js
@@ -1,4 +1,4 @@
-function showGlobalMessage(text, type = 'success') {
+export function showGlobalMessage(text, type = 'success') {
     const msg = document.getElementById('globalMessage');
     msg.textContent = text;
     msg.className = 'global-message' + (type === 'error' ? ' error' : '');
